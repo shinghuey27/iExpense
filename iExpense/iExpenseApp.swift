fbeba55c90c8b2bccc2e5f660df9b351b20907c9
shinghuey27 <shinghuey27@hotmail.com>
2024-06-05 17:43:24 +0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Shing Huey on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
